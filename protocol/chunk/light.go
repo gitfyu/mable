6c4aa172552726fe14671ae42d2fe838243c4677
fyu <>
2021-12-21 22:22:05 +0100

    Documentation for light.go

--- protocol/chunk/light.go
+++ protocol/chunk/light.go
@@ -1,6 +1,8 @@
 package chunk
 
 const (
-	LightDataSize = 2048
-	FullBright    = 15
+	// LightDataSize is the size of the array containing lighting data, referred to as NibbleArray in vanilla
+	LightDataSize = 16 * 16 * 16 / 2
+	// FullBright is the maximum light level
+	FullBright = 15
 )
